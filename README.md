29a26ab80acd48d288219d415600c79f78c205f2
felixmaier1989 <felixmaier1989@gmail.com>
2015-07-26 00:29:38 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+Yii2-RenderDual
+==========
+Make your actions compatible for Ajax requests
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist felixmaier1989/yii2-render-dual "*"
+```
+
+or add
+
+```
+"felixmaier1989/yii2-render-dual": "*"
+```
+
+to the require section of your `composer.json` file.
+
+
+Usage
+-----
+
+Once the extension is installed, simply use it in your code by  :
+
+```php
+<?= \yii2renderdual\AutoloadExample::widget(); ?>```
---
+++ tests/RenderDualBehaviorTest.php
@@ -0,0 +1,267 @@
+<?php
+
+namespace yii2renderdual;
+require 'RenderDualBehavior.php'; // @todo autoload with composer
+
+use Yii;
+use yiiunit\TestCase;
+use yii\web\Request;
+use yii\web\Controller;
+use yii\db\ActiveRecord;
+use RenderDualBehavior;
+
+/**
+ * Unit test for [[\yii2renderdual\RenderDualBehavior]].
+ * @see RenderDualBehavior
+ *
+ * @group behaviors
+ */
+class RenderDualBehaviorTest extends TestCase
+{
+
+    /**
+     * Mocks a Yii web application
+     * @param boolean $ajax
+     */
+    protected function _mockApplication($ajax) {
+        $this->mockWebApplication([
+            'components' => [
+                'session',
+                'request' => [
+                    'class' => $ajax ?
+                        '\yii2renderdual\AjaxRequest' :
+                        '\yii2renderdual\NonAjaxRequest',
+                ],
+            ],
+        ], '\yii\web\Application');
+    }
+
+    /**
+     * @test
+     */
+    public function testRenderDualNotController()
+    {
+        $controller = new \yii\db\ActiveRecord();
+        $controller->attachBehavior('RenderDual', new \yii2renderdual\RenderDualBehavior);
+        $this->setExpectedException('Exception');
+        $render = $controller->renderDual('view', ['foo' => 'bar'], 'foo bar');
+    }
+
+    /**
+     * @test
+     */
+    public function testRenderDualAjaxDataException()
+    {
+        $this->_mockApplication(true);
+
+        $controller = $this->getMock('\yii\web\Controller', ['render'], ['controllerTest', 'moduleTest']);
+        $controller->attachBehavior('RenderDual', new \yii2renderdual\RenderDualBehavior);
+        $controller
+            ->expects($this->once())
+            ->method('render')
+            ->with(
+                'view',
+                ['foo' => 'bar']
+            )
+            ->will($this->returnValue('<h1>Output</h1>'));
+
+        $this->setExpectedException('InvalidArgumentException');
+
+        $render = $controller->renderDual('view', ['foo' => 'bar'], 'foo bar');
+    }
+
+    /**
+     * @test
+     */
+    public function testRenderDualAjaxDataArray()
+    {
+        $this->_mockApplication(true);
+
+        $controller = $this->getMock('\yii\web\Controller', ['render'], ['controllerTest', 'moduleTest']);
+        $controller->attachBehavior('RenderDual', new \yii2renderdual\RenderDualBehavior);
+        $controller
+            ->expects($this->once())
+            ->method('render')
+            ->with(
+                'view',
+                ['foo' => 'bar']
+            )
+            ->will($this->returnValue('<h1>Output</h1>'));
+
+        $render = $controller->renderDual('view', ['foo' => 'bar'], ['alpha' => 'bravo']);
+
+        $this->assertEquals(['alpha' => 'bravo'], $render['data']);
+    }
+
+    /**
+     * @test
+     */
+    public function testRenderDualAjaxDataTrue()
+    {
+        $this->_mockApplication(true);
+
+        $controller = $this->getMock('\yii\web\Controller', ['render'], ['controllerTest', 'moduleTest']);
+        $controller->attachBehavior('RenderDual', new \yii2renderdual\RenderDualBehavior);
+        $controller
+            ->expects($this->once())
+            ->method('render')
+            ->with(
+                'view',
+                ['foo' => 'bar']
+            )
+            ->will($this->returnValue('<h1>Output</h1>'));
+
+        $render = $controller->renderDual('view', ['foo' => 'bar'], true);
+
+        $this->assertEquals(['foo' => 'bar'], $render['data']);
+    }
+
+    /**
+     * @test
+     */
+    public function testRenderDualAjaxDataFalse()
+    {
+        $this->_mockApplication(true);
+
+        $controller = $this->getMock('\yii\web\Controller', ['render'], ['controllerTest', 'moduleTest']);
+        $controller->attachBehavior('RenderDual', new \yii2renderdual\RenderDualBehavior);
+        $controller
+            ->expects($this->once())
+            ->method('render')
+            ->with(
+                'view',
+                ['foo' => 'bar']
+            )
+            ->will($this->returnValue('<h1>Output</h1>'));
+
+        $render = $controller->renderDual('view', ['foo' => 'bar'], false);
+
+        $this->assertEmpty($render['data']);
+    }
+
+    /**
+     * @test
+     */
+    public function testRenderDualAjax()
+    {
+        $this->_mockApplication(true);
+
+        $controller = $this->getMock('\yii\web\Controller', ['render'], ['controllerTest', 'moduleTest']);
+        $controller->attachBehavior('RenderDual', new \yii2renderdual\RenderDualBehavior);
+        $controller
+            ->expects($this->once())
+            ->method('render')
+            ->with(
+                'view',
+                ['foo' => 'bar']
+            )
+            ->will($this->returnValue('<h1>Output</h1>'));
+
+        \Yii::$app->session->setFlash('error', 'An error has occured');
+        \Yii::$app->session->setFlash('success', 'But everything is fine');
+
+        $render = $controller->renderDual('view', ['foo' => 'bar']);
+
+        $this->assertEquals(\yii\web\Response::FORMAT_JSON, \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON);
+        $this->assertEquals('<h1>Output</h1>', $render['output']);
+        $this->assertEquals('An error has occured', $render['flashes']['error']);
+        $this->assertEquals('But everything is fine', $render['flashes']['success']);
+    }
+
+    /**
+     * \yii\web\Controller::findLayoutFile() should return false
+     * @test
+     */
+    public function testRenderDualAjaxLayout()
+    {
+        $this->_mockApplication(true);
+
+        $view = $this->getMock('\yii\base\View', ['render']);
+        $view
+            ->expects($this->once())
+            ->method('render')
+            ->will($this->returnValue('<h1>Output</h1>'));
+
+        $controller = $this->getMock('\yii\web\Controller', ['findLayoutFile', 'getView'], ['controllerTest', 'moduleTest']);
+        $controller->attachBehavior('RenderDual', new \yii2renderdual\RenderDualBehavior);
+        $controller
+            ->expects($this->any())
+            ->method('getView')
+            ->will($this->returnValue($view));
+        $controller
+            ->expects($this->once())
+            ->method('findLayoutFile')
+            ->will($this->returnValue(false));
+
+        $render = $controller->renderDual('view', ['foo' => 'bar']);
+    }
+
+    /**
+     * \yii\web\Controller::findLayoutFile() should return a non empty value
+     * @test
+     */
+    public function testRenderDualNonAjaxLayout()
+    {
+        $this->_mockApplication(false);
+
+        $view = $this->getMock('\yii\base\View', ['render', 'renderFile']);
+        $view
+            ->expects($this->once())
+            ->method('render')
+            ->will($this->returnValue('<h1>Output</h1>'));
+        $view
+            ->expects($this->once())
+            ->method('renderFile')
+            ->will($this->returnValue('<h1>Layout</h1>'));
+
+        $controller = $this->getMock('\yii\web\Controller', ['findLayoutFile', 'getView'], ['controllerTest', 'moduleTest']);
+        $controller->attachBehavior('RenderDual', new \yii2renderdual\RenderDualBehavior);
+        $controller
+            ->expects($this->any())
+            ->method('getView')
+            ->will($this->returnValue($view));
+        $controller
+            ->expects($this->once())
+            ->method('findLayoutFile')
+            ->will($this->returnCallback(function($e){return (bool) $e;}));
+
+        $render = $controller->renderDual('view', ['foo' => 'bar']);
+    }
+
+    /**
+     * @test
+     */
+    public function testRenderDualNonAjax()
+    {
+        $this->_mockApplication(false);
+
+        $controller = $this->getMock('\yii\web\Controller', ['render'], ['controllerTest', 'moduleTest']);
+        $controller->attachBehavior('RenderDual', new \yii2renderdual\RenderDualBehavior);
+        $controller
+            ->expects($this->once())
+            ->method('render')
+            ->with(
+                'view',
+                ['foo' => 'bar']
+            )
+            ->will($this->returnValue('<h1>Output</h1>'));
+
+        $render = $controller->renderDual('view', ['foo' => 'bar']);
+
+        $this->assertEquals('<h1>Output</h1>', $render);
+    }
+}
+
+class AjaxRequest extends \yii\web\Request {
+    public function getIsAjax()
+    {
+        return true;
+    }
+}
+
+class NonAjaxRequest extends \yii\web\Request {
+    public function getIsAjax()
+    {
+        return false;
+    }
+}
